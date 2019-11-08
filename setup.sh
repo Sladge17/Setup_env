61d568d72d797d8a1237baaca22a68ef1f459f94
Jommo Thuy <jthuy@at-j4.21-school.ru>
2019-11-08 15:26:15 +0300

    add gitit.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-git add .
-if [ $# == 0 ]
+rm -rf .git
+git clone https://github.com/Sladge17/VSdebug_21.git
+cd VSdebug_21
+cp -r .vscode ./..
+cd ..
+rm -rf VSdebug_21
+echo .gitignore > .gitignore
+echo .vscode >> .gitignore
+echo *.sh >> .gitignore
+if [ $# == 1 ]
 then
-	git commit -m "No comment commit"
-else
-	git commit -m "$*"
+    git init
+    git remote add git_cloud $1
 fi
-git push github master
