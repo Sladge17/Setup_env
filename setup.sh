ae5e1be81ef2654ba0c5745b66c18ebf23a79c24
Jommo Thuy <jthuy@il-g3.msk.21-school.ru>
2020-07-24 15:43:01 +0300

    change setup.ch

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ then
     echo "Need two parameters:
     1 - repository address
     2 - project name"
-
 else
     rm -rf .git
     git clone https://github.com/Sladge17/VSdebug_21.git
@@ -15,6 +14,9 @@ else
     rm -rf VSdebug_21
     git init
     git remote add git_cloud $1
+    mv ../Setup_env ../$2
+    mv includes/project.h includes/$2.h
+    mv src/project.c src/$2.c
     rm -f setup.sh
 fi
 
